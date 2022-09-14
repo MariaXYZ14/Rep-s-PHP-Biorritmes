b9ad13bcf93e8f5ae1f03aebfb614a22509d6e14
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-14 17:48:51 +0200

    versio1

--- php/index.php
+++ php/index.php
@@ -1,37 +1,15 @@
 <!doctype html>
 <html lang="en">
-    
-    <?php 
-    include('header.php')
-   
-    ?>
-    <body>
-        
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    
-        <?php
-
-
-        if(isset($_POST["nom"])){
-            $_SESSION["nom"]=$_POST["nom"];
-            setCookie("nom",$_POST["nom"]);
-            $nom = $_POST["nom"];
-            echo "<h2>Benvingut $nom </h2>";
-        }else{
-            if(isset($_SESSION["nom"])){
-                echo "<h2>Benvingut " .$_SESSION["nom"] . "</h2>"; 
-                $nom=$_COOKIE["nom"];
-            }
-            
-        }
-        
-        ?>
-
-    <?php
-    include('footer.php')
-
-    ?>
-
-   </body>
-
- </html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/estil.css"/>
+    <title>INDEX</title>
+    <form action="accion.php" method="post">
+        <p>Nombre: <input type="text" name="nombre" /></p>
+        <p>Edad: <input type="date"  value="2022-01-01" min="1926-04-21" max="2022-01-01" name="edad" /></p>
+        <p><input type="submit" /></p>
+    </form>
+   </head>
+</html>
