00fe772f744e7785ad3462b2ba6c48a5ddfec512
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-15 09:12:22 +0200

    versio1

--- php/resultats.php
+++ php/resultats.php
@@ -1,11 +1,12 @@
 <!DOCTYPE html>
 <html lang="en">
+<head>
+<link href="../css/estil.css?uuid=<?php echo uniqid();?>" type="text/css" rel="stylesheet">
+</head>
 <body>
 <?php 
         include 'biorritmes.php';
-        echo '<link href="../css/estil.css" type="text/css" rel="stylesheet">';
-      //  echo '<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">';
-
+        echo '<link href="../css/estil.css?uuid=<?php echo uniqid();?>" type="text/css" rel="stylesheet">';
 
           $nom="";
 
@@ -44,6 +45,7 @@
 <progress id="file" max="100" value='<?php echo abs($cicle_intelectual*100)?>'></progress><br>
 
 </body>
+
 </html>
 
                
