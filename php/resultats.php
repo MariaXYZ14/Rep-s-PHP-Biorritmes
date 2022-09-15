2b826aa8e5bcf7adf9b720dcd7091fd02d05a374
MariaXYZ14 <mariaxyz14@gmail.com>
2022-09-15 13:06:51 +0200

    versio1

--- php/resultats.php
+++ php/resultats.php
@@ -3,14 +3,16 @@
 <head>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link href="../css/estil.css?uuid=<?php echo uniqid();?>" type="text/css" rel="stylesheet">
+<link href="../css/estil.css" type="text/css" rel="stylesheet">
 <title>Biorritmes</title>
 </head>
 <body>
 <?php 
-        include 'biorritmes.php';
-        echo '<link href="../css/estil.css?uuid=<?php echo uniqid();?>" type="text/css" rel="stylesheet">';
 
+
+        include 'biorritmes.php';
+        echo '<link href="../css/estil.css" type="text/css" rel="stylesheet">';
+        
           $nom="";
 
           if(isset($_POST['nom'])){
@@ -38,15 +40,18 @@
           $newClass3='';
 
           if($cicle_fisic < 0){
+
             $newClass = 'barraFisicaNegativa';
       
           }
 
           if($cicle_emocional < 0){
+
             $newClass2 = 'barraEmocionalNegativa';
       
           }
           if($cicle_intelectual < 0){
+
             $newClass3 = 'barraIntelectualNegativa';
       
           }
@@ -54,7 +59,6 @@
           echo '<br>El cicle físic és de ' . $cicle_fisic;
           echo '<br>El cicle emocional és de ' . $cicle_emocional;
           echo '<br>El cicle intelectual és de ' . $cicle_intelectual . '<br>';
-
 
 
 ?>  
@@ -65,7 +69,9 @@
 <progress class="barra <?php echo $newClass2;?>" max="100" value='<?php echo abs($cicle_emocional*100)?>'></progress><br>
 <label for="file">Cicle Intelectual <?php echo ($cicle_intelectual*100)?> %</label><br>
 <progress  class="barra <?php echo $newClass3;?>" max="100" value='<?php echo abs($cicle_intelectual*100)?>'></progress><br>
+
 </body>
+
 </html>
 
                
